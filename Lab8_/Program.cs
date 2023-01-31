c2174d9562722756636b1934432476f4e2c46d6a
romvasko <romvasko@yandex.by>
2023-01-31 14:48:51 +0300

    done1

--- Lab8_/Program.cs
+++ Lab8_/Program.cs
@@ -23,6 +23,6 @@ app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 //app.MapGet("{controller=Home}/{action=Index}/{id?}", () => "");
 app.Run();
